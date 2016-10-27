bbc2a57392da01498e24fe9467376fee6c8db9dd
3miliomc <3milio.mc@gmail.com>
2016-10-27 17:52:42 -0300

    bran

---
+++ WebSiteTest.FunctionalTest/SeleniumTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSiteTest.FunctionalTest
+{
+    [TestClass]
+    public class SeleniumTests
+    {
+        private string baseURL = "http://191.235.87.105/SiteTestC/";
+        private RemoteWebDriver driver;
+        private string browser;
+
+        public TestContext Contexto { get; set; }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        [Priority(19)]
+        [Owner("FireFox")] //Using Owner as Category trait is not supported by the DTA Task
+        public void RemoteSelenium()
+        {
+            DesiredCapabilities capability = DesiredCapabilities.Firefox();
+            Uri server = new Uri("http://<YourGridserver>:80/wd/hub");
+            this.driver = new RemoteWebDriver(server, capability);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(this.baseURL);
+            driver.FindElementById("search - box").Clear();
+            driver.FindElementById("search - box").SendKeys("tire");
+            //do other Selenium things here!
+        }
+    }
+}
