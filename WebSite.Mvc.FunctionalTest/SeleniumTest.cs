aaf5785c1370948836c0762b81953f8acdb26154
3miliomc <3milio.mc@gmail.com>
2016-10-28 14:17:32 -0300

    neu

--- WebSite.Mvc.FunctionalTest/SeleniumTest.cs
+++ WebSite.Mvc.FunctionalTest/SeleniumTest.cs
@@ -1,26 +1,27 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
-namespace WebSiteTest.FunctionalTest
+namespace WebSite.Mvc.FunctionalTest
 {
-    [TestClass]
-    public class SeleniumTests
+        public class SeleniumTest: IClassFixture<FirefoxDriver>
     {
         private string baseURL = "http://191.235.87.105/SiteTestC/";
         private RemoteWebDriver driver;
         private string browser;
 
-        public TestContext Contexto { get; set; }
+        //public TestContext Contexto { get; set; }
 
-        [TestMethod]
-        [TestCategory("Selenium")]
-        [Priority(19)]
-        [Owner("FireFox")] //Using Owner as Category trait is not supported by the DTA Task
+        //[TestMethod]
+        //[TestCategory("Selenium")]
+        //[Priority(19)]
+        //[Owner("FireFox")] //Using Owner as Category trait is not supported by the DTA Task
+        [Fact]
         public void RemoteSelenium()
         {
             DesiredCapabilities capability = DesiredCapabilities.Firefox();
@@ -29,7 +30,7 @@ namespace WebSiteTest.FunctionalTest
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(this.baseURL);
             //driver.FindElementById("search - box").Clear();
-            driver.FindElementById("link-contact").Click();
+            driver.FindElementById("search - box").SendKeys("tire");
             //do other Selenium things here!
         }
     }
