f044143aa62ad5d11488dd77cdade39dd80b3430
3miliomc <3milio.mc@gmail.com>
2016-11-02 09:53:47 -0300

    oki

--- WebSite.Mvc/Helpers/CalculadoraFinanciera.cs
+++ WebSite.Mvc/Helpers/CalculadoraFinanciera.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebSite.Mvc.Models;
 
 namespace WebSite.Mvc.Helpers
@@ -14,29 +11,30 @@ namespace WebSite.Mvc.Helpers
         {
             var lista = new List<AmortizacionUnitaria>();
 
-            var endingBalance = prestamo - pie;
-            var rate = tasa / 1200.0;
-            var monthly = rate > 0 ? ((rate + rate / (Math.Pow(1 + rate, plazo) - 1)) * endingBalance) : endingBalance / plazo;
-            
-            var count = 1;
-            while (count <= mensual)
-            {
-                var interestPaid = endingBalance * rate;
-                var principlePaid = mensual - interestPaid;
-                endingBalance -= principlePaid;
+            //var endingBalance = prestamo - pie;
+            //var rate = tasa / 1200.0;
+            //var monthly = rate > 0 ? ((rate + rate / (Math.Pow(1 + rate, plazo) - 1)) * endingBalance) : endingBalance / plazo;
 
-                var pago = new AmortizacionUnitaria()
-                {
-                    Numero = count,
-                    PagoInicial = String.Format("{0:C}", interestPaid),
-                    CapitalPagado = String.Format("{0:C}",
-                          principlePaid),
-                    Saldo = String.Format("{0:C}", endingBalance)
-                };
+            //var count = 1;
+            //while (count <= mensual)
+            //{
+            //    var interestPaid = endingBalance * rate;
+            //    var principlePaid = mensual - interestPaid;
+            //    endingBalance -= principlePaid;
 
-                lista.Add(pago);
-                count++;
-            }
+            //    var pago = new AmortizacionUnitaria()
+            //    {
+            //        Numero = count,
+            //        PagoInicial = String.Format("{0:C}", interestPaid),
+            //        CapitalPagado = String.Format("{0:C}",
+            //              principlePaid),
+            //        Saldo = String.Format("{0:C}", endingBalance)
+            //    };
+
+            //    lista.Add(pago);
+            //    count++;
+            //}
+            lista.Add(new AmortizacionUnitaria() { Numero = 1, CapitalPagado = "2000", PagoInicial = "100", Saldo = "0" });
             return lista;
         }
     }
